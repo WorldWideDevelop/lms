fd5817223542b734049a875ad5cbed49b7250a34
AdrianTan914 <bridleland@gmail.com>
2021-09-26 22:15:01 -0300

    MODIFY HomePage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,17 @@
-import React from "react";
-import HeroSection from "../components/HeroSection/HeroSection";
+import React from 'react';
+import HeroSection from '../components/HeroSection/HeroSection';
+import Video from '../assets/videos/hero_video.mp4';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
-  return <HeroSection />;
+	const [t, i18n] = useTranslation('global');
+	return (
+		<HeroSection
+			Video={Video}
+			Title={t('hero.title')}
+			Paragraph={t('hero.paragraph')}
+		/>
+	);
 };
 
 export default HomePage;
